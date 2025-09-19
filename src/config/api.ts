20374e25831d219ea536dd3b6250c361c939ced5
Rushabh <=>
2025-09-19 20:33:59 +0530

    Changed the API URL to domain name

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: "http://89.116.34.13:13000",
+  // BASE_URL: "http://89.116.34.13:13000",
+  BASE_URL: "http://admin.yourcustom.jewelry",
   // TODO: Update this token when it expires - current token expires on 2025-01-30
   AUTH_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInJvbGVOYW1lIjoiYWRtaW4iLCJpYXQiOjE3NTQ4MzIyNDIsImV4cCI6MzMzMTI0MzIyNDJ9.knPDyJFwJINHlzdC43W2HVO28aotjJQqHKiLaOFqmvQ"
 };
