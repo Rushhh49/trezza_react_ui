a3bcdbe44a12fdea224d4c880bf3834684055c38
Rushabh Nagda <rushabhnagda@Rushabh-Nagda.local>
2025-11-02 23:37:08 +0530

    fixed desktop view

--- src/pages/ItemDetailPage.tsx
+++ src/pages/ItemDetailPage.tsx
@@ -722,7 +722,7 @@ const ItemDetailPage: React.FC = () => {
           </div>
 
           {/* Details */}
-          <div className="flex-1 w-full max-w-2xl mx-auto lg:mx-0">
+          <div className="flex-1 w-full mx-auto lg:mx-0">
             <Card className="p-6 bg-white border border-gray-200 shadow-sm mb-6">
               {/* <h3 className="text-lg font-medium text-gray-900 font-['Playfair_Display']">Details</h3> */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
